c5fd7463b892a0f1521807851b9eff0346e08813
exhq <exhq@nea.moe>
2024-02-27 20:25:18 +0330

    license headers

--- src/plugins/superReactionTweaks/index.ts
+++ src/plugins/superReactionTweaks/index.ts
@@ -1,6 +1,7 @@
 /*
- * Vencord, a Discord client mod
- * Copyright (c) 2023 Vendicated, ant0n, FieryFlames and contributors
+ * Reshigged, a Discord client mod
+ * forked from vencord
+ * Copyright (c) 2024 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
